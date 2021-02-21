e2173912ee54ca97183f7dd7daf3d014141216ac
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-21 21:17:24 +0530

    first commit

---
+++ UserValidatorTest.java
@@ -0,0 +1,26 @@
+package com.uservalidator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserValidatorTest {
+
+    @Test
+    public void welcome() {
+        System.out.println("Welcome");
+    }
+
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturntrue() {
+        UserValidator userValidator = new UserValidator();
+        boolean firstName = userValidator.validFirstName("Omprasad");
+        Assertions.assertTrue(firstName);
+    }
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturnfalse() {
+        UserValidator userValidator = new UserValidator();
+        boolean firstName = userValidator.validFirstName("omprasad");
+        Assertions.assertFalse(firstName);
+    }
+
+}
